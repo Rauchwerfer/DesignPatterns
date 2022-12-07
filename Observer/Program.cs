8065dcea50826860ef301b5c62f3f2b0a4a32dd6
Rauchwerfer <dmitrii.cherkasov.030298@gmail.com>
2022-12-07 10:39:55 +0200

    Add Observer Pattern Example

---
+++ Observer/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace Observer
+{
+    /// <summary>
+    /// Observer Design Pattern
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Configure Observer pattern
+            ConcreteSubject s = new ConcreteSubject();
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            // Change subject and notify observers
+            s.SubjectState = "ABC";
+            //s.Notify();
+
+            s.SubjectState = "DEF";
+            //s.Notify();
+            
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    public abstract class Subject
+    {
+        private List<Observer> observers = new List<Observer>();
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+        public void Detach(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+        public void Notify()
+        {
+            foreach (Observer o in observers)
+            {
+                o.Update();
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteSubject' class
+    /// </summary>
+    public class ConcreteSubject : Subject
+    {
+        private string subjectState;
+        // Gets or sets subject state
+        public string SubjectState
+        {
+            get { return subjectState; }
+            set { 
+                subjectState = value;
+                // Not necessary to notify there
+                Notify();
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'Observer' abstract class
+    /// </summary>
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+
+    /// <summary>
+    /// The 'ConcreteObserver' class
+    /// </summary>
+    public class ConcreteObserver : Observer
+    {
+        private string name;
+        private string observerState;
+        private ConcreteSubject subject;
+        // Constructor
+        public ConcreteObserver(
+            ConcreteSubject subject, string name)
+        {
+            this.subject = subject;
+            this.name = name;
+        }
+        public override void Update()
+        {
+            observerState = subject.SubjectState;
+            Console.WriteLine("Observer {0}'s new state is {1}",
+                name, observerState);
+        }
+        // Gets or sets subject
+        public ConcreteSubject Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+    }
+}
